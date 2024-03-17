48504ac49799e40bce2181aae2dc994c3bd07a5f
DenisPonomarev22 <ponomarevd200522@gmail.com>
2024-03-17 14:48:31 +0300

    Создание миграций

---
+++ Hospital.Domain/HospitalContext.cs
@@ -0,0 +1,23 @@
+﻿using Hospital.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Domain;
+
+public class HospitalContext : DbContext
+{
+    public DbSet<Departament> Departaments { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Hospitalization> Hospitalizations{ get; set; }
+    public DbSet<Measure> Measures { get; set; }
+    public DbSet<MeasureType> MeasureType { get; set; }
+    public DbSet<MedicalCard> MedicalCards { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Policy> Policies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Hospital2;Trusted_Connection=True;");
+    }
+
+
+}
